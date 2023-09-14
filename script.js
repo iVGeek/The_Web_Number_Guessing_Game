4d80628b064964b9ceeb3263ab43aa341bfe322e
iVGeek <48223278+iVGeek@users.noreply.github.com>
2023-09-14 16:22:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
+// JavaScript for Guess the Number game
+
 // Initialize game variables
 let secretNumber;
 let attemptsLeft;
 let isGameOver = false;
 let leaderboard = [];
+let gameMode = 'single'; // Default to single player
 
 // Function to start a new game
 function startNewGame() {
-    secretNumber = generateRandomNumber(1, 100);
-    attemptsLeft = 4;
+    // Retrieve the selected game mode
+    const gameModeRadios = document.getElementsByName('game-mode');
+    for (const radio of gameModeRadios) {
+        if (radio.checked) {
+            gameMode = radio.value;
+            break;
+        }
+    }
+
+    // Adjust game settings based on the selected mode
+    if (gameMode === 'single') {
+        secretNumber = generateRandomNumber(1, 100);
+        attemptsLeft = 4;
+    } else if (gameMode === 'multi') {
+        // Adjust settings for multiplayer mode here
+    }
+
     isGameOver = false;
     document.getElementById('result-message').textContent = '';
     document.getElementById('attempts-left').textContent = `Attempts left: ${attemptsLeft}`;
@@ -20,53 +38,3 @@ function generateRandomNumber(min, max) {
 }
 
 // Function to handle the "Guess" button click
-function handleGuessClick() {
-    if (isGameOver) {
-        return;
-    }
-
-    const guessInput = document.getElementById('guess-input');
-    const guess = parseInt(guessInput.value);
-
-    if (isNaN(guess) || guess < 1 || guess > 100) {
-        document.getElementById('result-message').textContent = 'Invalid guess. Please enter a number between 1 and 100.';
-        return;
-    }
-
-    attemptsLeft--;
-
-    if (guess === secretNumber) {
-        endGame(true);
-    } else if (attemptsLeft === 0) {
-        endGame(false);
-    } else {
-        document.getElementById('attempts-left').textContent = `Attempts left: ${attemptsLeft}`;
-        document.getElementById('result-message').textContent = guess < secretNumber ? 'Too low!' : 'Too high!';
-    }
-}
-
-// Function to end the game and display the result
-function endGame(isWinner) {
-    isGameOver = true;
-    const resultMessage = document.getElementById('result-message');
-    resultMessage.textContent = isWinner ? 'Congratulations! You guessed the number!' : `Game over. The number was ${secretNumber}.`;
-    leaderboard.push({ attempts: 5 - attemptsLeft, isWinner });
-    showLeaderboard();
-}
-
-// Function to display the leaderboard
-function showLeaderboard() {
-    leaderboard.sort((a, b) => a.attempts - b.attempts);
-    const leaderboardContainer = document.getElementById('leaderboard');
-    leaderboardContainer.innerHTML = '<h2>Leaderboard</h2>';
-    leaderboard.forEach((entry, index) => {
-        leaderboardContainer.innerHTML += `<p>${index + 1}. Attempts: ${entry.attempts}, ${entry.isWinner ? 'Winner' : 'Loser'}</p>`;
-    });
-}
-
-// Event listeners
-document.getElementById('start-button').addEventListener('click', startNewGame);
-document.getElementById('guess-button').addEventListener('click', handleGuessClick);
-
-// Start a new game when the page loads
-startNewGame();
