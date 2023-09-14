70322744b9b55e5ad15f4f2d905a2d0ba588ac0a
iVGeek <48223278+iVGeek@users.noreply.github.com>
2023-09-14 16:41:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,60 +5,21 @@ let secretNumber;
 let attemptsLeft;
 let isGameOver = false;
 let leaderboard = [];
-let gameMode = 'single';
+let isDarkMode = false;
 
 // Function to start a new game
 function startNewGame() {
-    // Retrieve the selected game mode
-    const gameModeRadios = document.getElementsByName('game-mode');
-    for (const radio of gameModeRadios) {
-        if (radio.checked) {
-            gameMode = radio.value;
-            break;
-        }
-    }
+    const playerName = getPlayerName();
+    if (!playerName) return;
 
-    // Retrieve the selected game level
-    const gameLevelRadios = document.getElementsByName('game-level');
-    let minRange, maxRange, maxAttempts;
-    for (const radio of gameLevelRadios) {
-        if (radio.checked) {
-            const level = radio.value;
-            if (level === 'easy') {
-                minRange = 1;
-                maxRange = 30;
-                maxAttempts = 10;
-            } else if (level === 'medium') {
-                minRange = 1;
-                maxRange = 50;
-                maxAttempts = 6;
-            } else if (level === 'hard') {
-                minRange = 1;
-                maxRange = 100;
-                maxAttempts = 4;
-            }
-            break;
-        }
-    }
-
-    // Prompt for the player's name
-    const playerName = prompt('Enter your name to start the game:');
-    if (!playerName) {
-        return; // Exit if the player cancels or leaves the name blank
-    }
-
-    // Adjust game settings based on the selected mode and level
-    if (gameMode === 'single') {
-        secretNumber = generateRandomNumber(minRange, maxRange);
-        attemptsLeft = maxAttempts;
-    } else if (gameMode === 'multi') {
-        // Adjust settings for multiplayer mode here
-    }
-
+    secretNumber = generateRandomNumber(1, 100);
+    attemptsLeft = getAttemptsForLevel();
     isGameOver = false;
     document.getElementById('result-message').textContent = '';
     document.getElementById('attempts-left').textContent = `Attempts left: ${attemptsLeft}`;
     document.getElementById('guess-input').value = '';
+    document.getElementById('game-controls').style.display = 'none';
+    document.getElementById('gameplay').style.display = 'block';
 }
 
 // Function to generate a random number between min and max (inclusive)
@@ -97,8 +58,10 @@ function endGame(isWinner) {
     isGameOver = true;
     const resultMessage = document.getElementById('result-message');
     resultMessage.textContent = isWinner ? 'Congratulations! You guessed the number!' : `Game over. The number was ${secretNumber}.`;
-    leaderboard.push({ playerName, attempts: 5 - attemptsLeft, isWinner });
+    leaderboard.push({ attempts: 5 - attemptsLeft, isWinner });
     showLeaderboard();
+    document.getElementById('game-controls').style.display = 'block';
+    document.getElementById('gameplay').style.display = 'none';
 }
 
 // Function to display the leaderboard
@@ -107,13 +70,60 @@ function showLeaderboard() {
     const leaderboardContainer = document.getElementById('leaderboard');
     leaderboardContainer.innerHTML = '<h2>Leaderboard</h2>';
     leaderboard.forEach((entry, index) => {
-        leaderboardContainer.innerHTML += `<p>${index + 1}. ${entry.playerName}: Attempts - ${entry.attempts}, ${entry.isWinner ? 'Winner' : 'Loser'}</p>`;
+        leaderboardContainer.innerHTML += `<p>${index + 1}. Attempts: ${entry.attempts}, ${entry.isWinner ? 'Winner' : 'Loser'}</p>`;
     });
+}
+
+// Function to get the player's name
+function getPlayerName() {
+    const playerName = prompt('Enter your name:');
+    return playerName;
+}
+
+// Function to get the number of attempts based on the selected level
+function getAttemptsForLevel() {
+    const levelRadios = document.getElementsByName('game-level');
+    for (const radio of levelRadios) {
+        if (radio.checked) {
+            const level = radio.value;
+            switch (level) {
+                case 'easy':
+                    return 8;
+                case 'medium':
+                    return 6;
+                case 'hard':
+                    return 4;
+                default:
+                    return 8; // Default to easy level
+            }
+        }
+    }
 }
 
 // Event listeners
 document.getElementById('start-button').addEventListener('click', startNewGame);
 document.getElementById('guess-button').addEventListener('click', handleGuessClick);
+
+// Toggle Dark Mode
+document.getElementById('toggle-dark-mode').addEventListener('click', toggleDarkMode);
+
+// Function to toggle dark/light mode
+function toggleDarkMode() {
+    isDarkMode = !isDarkMode;
+    const body = document.body;
+    const header = document.querySelector('header');
+    const footer = document.querySelector('footer');
+
+    if (isDarkMode) {
+        body.classList.add('dark-mode');
+        header.classList.add('dark-mode');
+        footer.classList.add('dark-mode');
+    } else {
+        body.classList.remove('dark-mode');
+        header.classList.remove('dark-mode');
+        footer.classList.remove('dark-mode');
+    }
+}
 
 // Start a new game when the page loads
 startNewGame();
