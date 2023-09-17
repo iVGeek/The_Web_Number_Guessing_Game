298319c43ee368c9bf031d66793283abdf576806
iVGeek <48223278+iVGeek@users.noreply.github.com>
2023-09-18 01:30:53 +0300

    added game modes (single player and multiplayer)

--- script.js
+++ script.js
@@ -7,7 +7,11 @@ const HARD_MIN = 1;
 const HARD_MAX = 500;
 
 // Game variables
-let min, max, targetNumber, remainingAttempts, timer, bestScore;
+let min, max, targetNumber, remainingAttempts, timer, bestScore, currentPlayer;
+const playerGuesses = {
+    1: [],
+    2: [],
+};
 
 const difficultySelect = document.getElementById('difficulty');
 const timeLimitInput = document.getElementById('timeLimit');
@@ -23,7 +27,9 @@ const bestScoreDisplay = document.getElementById('bestScore');
 startButton.addEventListener('click', startGame);
 
 // Event listener for the guess button
-guessSubmit.addEventListener('click', checkGuess);
+guessSubmit.addEventListener('click', function () {
+    checkGuess(currentPlayer);
+});
 
 // Start the game
 function startGame() {
@@ -43,6 +49,9 @@ function startGame() {
     targetNumber = generateRandomNumber(min, max);
     remainingAttempts = maxAttemptsByDifficulty(selectedDifficulty);
     bestScore = parseInt(localStorage.getItem(selectedDifficulty)) || Infinity;
+    currentPlayer = 1;
+    playerGuesses[1] = [];
+    playerGuesses[2] = [];
 
     guessField.value = '';
     message.textContent = '';
@@ -88,59 +97,44 @@ function startTimer() {
         timeLeft.textContent = timeRemaining;
 
         if (timeRemaining === 0) {
-            endGame(false);
+            endGame(0); // Time's up, declare a tie
         }
     }, 1000);
 }
 
 // Check the user's guess
-function checkGuess() {
+function checkGuess(player) {
     const userGuess = parseInt(guessField.value);
 
     if (isNaN(userGuess) || userGuess < min || userGuess > max) {
-        message.textContent = `Please enter a valid number between ${min} and ${max}.`;
+        message.textContent = `Player ${player}, please enter a valid number between ${min} and ${max}.`;
         return;
     }
 
-    remainingAttempts--;
-    attempts.textContent = maxAttemptsByDifficulty(difficultySelect.value) - remainingAttempts;
+    playerGuesses[player].push(userGuess);
 
-    if (userGuess === targetNumber) {
-        endGame(true);
+    // Check if the current player has guessed correctly or come closest
+    if (userGuess === targetNumber || Math.abs(userGuess - targetNumber) < Math.abs(playerGuesses[3 - player][playerGuesses[3 - player].length - 1] - targetNumber)) {
+        endGame(player);
     } else {
-        if (userGuess < targetNumber) {
-            message.textContent = 'Try a higher number.';
-        } else {
-            message.textContent = 'Try a lower number.';
-        }
-
+        message.textContent = `Player ${player}, your guess is recorded. Next player's turn.`;
         guessField.value = '';
         guessField.focus();
-
-        if (remainingAttempts === 0) {
-            endGame(false);
-        }
+        currentPlayer = 3 - currentPlayer; // Switch players
     }
 }
 
-// End the game
-function endGame(isWinner) {
+// End the game and declare the winner
+function endGame(winner) {
     clearInterval(timer);
     guessField.setAttribute('disabled', 'disabled');
     guessSubmit.setAttribute('disabled', 'disabled');
     timeLimitInput.removeAttribute('disabled');
     startButton.removeAttribute('disabled');
 
-    if (isWinner) {
-        message.textContent = `Congratulations! You guessed the number in ${maxAttemptsByDifficulty(difficultySelect.value) - remainingAttempts} attempts.`;
-
-        // Update best score if necessary
-        if (remainingAttempts < bestScore) {
-            bestScore = remainingAttempts;
-            localStorage.setItem(difficultySelect.value, bestScore);
-            bestScoreDisplay.textContent = bestScore;
-        }
+    if (winner === 1 || winner === 2) {
+        message.textContent = `Player ${winner} wins! The correct number was ${targetNumber}.`;
     } else {
-        message.textContent = `Out of time. The correct number was ${targetNumber}.`;
+        message.textContent = `It's a tie! The correct number was ${targetNumber}.`;
     }
 }
