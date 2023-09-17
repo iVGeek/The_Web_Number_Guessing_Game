3a5ff7d0265ecf97a973d9b4890219fc30bae503
iVGeek <48223278+iVGeek@users.noreply.github.com>
2023-09-17 18:34:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,102 @@
-// Initialize game variables
+// Global variables
+let player1Name = "";
+let player2Name = "";
+let currentPlayer = "";
+let currentLevel = {};
+let secretNumber = 0;
+let attemptsLeft = 0;
+let isMultiplayer = false;
 let leaderboard = [];
-let currentLevel;
-let secretNumber;
-let attemptsLeft;
-
-// Define game levels
-class GameLevel {
-    constructor(name, min, max, maxAttempts, color) {
-        this.name = name;
-        this.min = min;
-        this.max = max;
-        this.maxAttempts = maxAttempts;
-        this.color = color;
-    }
-}
 
 // Function to display the welcome screen
 function displayWelcomeScreen() {
-    const welcomeMessage = document.getElementById('welcome-message');
-    welcomeMessage.textContent = "Welcome to Guess the Number!\nGame developed by Your Name";
-}
-
-// Function to select a game level
-function selectGameLevel() {
-    const levelSelect = document.getElementById('level-dropdown');
-
-    // Listen for level selection
-    levelSelect.addEventListener('change', function () {
-        const selectedLevel = levelSelect.value;
-        switch (selectedLevel) {
-            case 'easy':
-                currentLevel = new GameLevel("Easy", 1, 10, 8, "green");
-                break;
-            case 'medium':
-                currentLevel = new GameLevel("Medium", 1, 50, 6, "yellow");
-                break;
-            case 'hard':
-                currentLevel = new GameLevel("Hard", 1, 100, 4, "red");
-                break;
-            default:
-                currentLevel = new GameLevel("Medium", 1, 50, 4, "yellow");
-                break;
-        }
+    const welcomeMessage = document.createElement("h1");
+    welcomeMessage.textContent = "Welcome to Guess the Number!";
+    const playerNameInput = document.createElement("input");
+    playerNameInput.setAttribute("type", "text");
+    playerNameInput.setAttribute("id", "player-name");
+    playerNameInput.setAttribute("placeholder", "Enter your name");
+    const gameModeButtons = document.createElement("div");
+    gameModeButtons.setAttribute("id", "game-mode");
+    const singlePlayerButton = document.createElement("button");
+    singlePlayerButton.textContent = "Single Player";
+    singlePlayerButton.addEventListener("click", () => {
+        isMultiplayer = false;
         startGame();
     });
+    const multiplayerButton = document.createElement("button");
+    multiplayerButton.textContent = "Multiplayer";
+    multiplayerButton.addEventListener("click", () => {
+        isMultiplayer = true;
+        askForPlayerNames();
+    });
+
+    // Clear existing content and add the welcome screen elements
+    const gameArea = document.getElementById("game-area");
+    gameArea.innerHTML = "";
+    gameArea.appendChild(welcomeMessage);
+    gameArea.appendChild(playerNameInput);
+    gameArea.appendChild(gameModeButtons);
+    gameModeButtons.appendChild(singlePlayerButton);
+    gameModeButtons.appendChild(multiplayerButton);
+}
+
+// Function to ask for player names in multiplayer mode
+function askForPlayerNames() {
+    const playerNameInput = document.getElementById("player-name");
+    player1Name = playerNameInput.value;
+    playerNameInput.value = "";
+    playerNameInput.placeholder = "Enter Player 2's name";
+    playerNameInput.addEventListener("keyup", (event) => {
+        if (event.key === "Enter") {
+            player2Name = playerNameInput.value;
+            startGame();
+        }
+    });
+}
+
+// Function to toggle dark mode
+function toggleDarkMode() {
+    const body = document.body;
+    body.classList.toggle("dark-mode");
 }
 
 // Function to start the game
 function startGame() {
+    // Reset game state
+    currentPlayer = player1Name;
+    currentLevel = {
+        name: "Easy",
+        min: 1,
+        max: 10,
+        maxAttempts: 5,
+    };
     secretNumber = generateRandomNumber(currentLevel.min, currentLevel.max);
     attemptsLeft = currentLevel.maxAttempts;
 
-    // Display game elements
-    const levelSelect = document.getElementById('level-select');
-    const gameElements = document.getElementById('game-elements');
-    levelSelect.style.display = 'none';
-    gameElements.style.display = 'block';
-
-    // Clear previous messages
-    const messageElement = document.getElementById('message');
-    messageElement.textContent = '';
-
-    // Display attempts left
-    const attemptsLeftElement = document.getElementById('attempts-left');
+    // Create game elements
+    const gameArea = document.getElementById("game-area");
+    gameArea.innerHTML = "";
+    const gameHeader = document.createElement("h2");
+    gameHeader.textContent = "Guess the Number";
+    const messageElement = document.createElement("p");
+    messageElement.setAttribute("id", "message");
+    const attemptsLeftElement = document.createElement("p");
+    attemptsLeftElement.setAttribute("id", "attempts-left");
     attemptsLeftElement.textContent = `Attempts left: ${attemptsLeft}`;
+    const guessInput = document.createElement("input");
+    guessInput.setAttribute("type", "number");
+    guessInput.setAttribute("id", "guess-input");
+    guessInput.setAttribute("placeholder", "Enter your guess");
+    const guessButton = document.createElement("button");
+    guessButton.textContent = "Guess";
+    guessButton.addEventListener("click", handleGuess);
+
+    gameArea.appendChild(gameHeader);
+    gameArea.appendChild(messageElement);
+    gameArea.appendChild(attemptsLeftElement);
+    gameArea.appendChild(guessInput);
+    gameArea.appendChild(guessButton);
 }
 
 // Function to generate a random number between min and max (inclusive)
@@ -73,99 +106,55 @@ function generateRandomNumber(min, max) {
 
 // Function to handle user guesses
 function handleGuess() {
-    const guessInput = document.getElementById('guess-input');
-    const guess = parseInt(guessInput.value);
-
-    if (isNaN(guess) || guess < currentLevel.min || guess > currentLevel.max) {
-        displayMessage('Invalid guess. Please enter a number between ' + currentLevel.min + ' and ' + currentLevel.max + '.');
-        return;
-    }
-
-    attemptsLeft--;
-
-    if (guess === secretNumber) {
-        endGame(true);
-    } else if (attemptsLeft === 0) {
-        endGame(false);
-    } else {
-        displayMessage(guess < secretNumber ? 'Too low!' : 'Too high!');
-        displayAttemptsLeft();
-    }
+    // Implement guess handling logic here
+    // Check if the guess is correct, too low, or too high
+    // Update the message, attempts left, and end the game if needed
 }
 
 // Function to end the game
 function endGame(isWinner) {
-    const messageElement = document.getElementById('message');
-    if (isWinner) {
-        messageElement.textContent = 'Congratulations! You guessed the number!';
-    } else {
-        messageElement.textContent = 'Game over. The number was ' + secretNumber + '.';
-    }
-
-    // Update leaderboard
-    updateLeaderboard('Player Name', currentLevel.maxAttempts - attemptsLeft, currentLevel.name);
-
-    // Hide game elements
-    const gameElements = document.getElementById('game-elements');
-    gameElements.style.display = 'none';
-
-    // Show level select again
-    const levelSelect = document.getElementById('level-select');
-    levelSelect.style.display = 'block';
-}
-
-// Function to display a message to the user
-function displayMessage(message) {
-    const messageElement = document.getElementById('message');
-    messageElement.textContent = message;
-}
-
-// Function to display remaining attempts
-function displayAttemptsLeft() {
-    const attemptsLeftElement = document.getElementById('attempts-left');
-    attemptsLeftElement.textContent = `Attempts left: ${attemptsLeft}`;
+    // Implement game ending logic here
+    // Update the message to show the game result
+    // Update the leaderboard
+    // Display the leaderboard and offer to play again
 }
 
 // Function to update the leaderboard
 function updateLeaderboard(player, score, level) {
-    leaderboard.push({ player, score, level });
-    leaderboard.sort((a, b) => a.score - b.score);
-    if (leaderboard.length > 5) {
-        leaderboard.shift(); // Remove the lowest score if leaderboard size exceeds the maximum
-    }
-    displayLeaderboard();
+    // Implement leaderboard update logic here
+    // Add the player, score, and level to the leaderboard array
+    // Sort the leaderboard based on scores
+    // Keep only the top scores (you can limit the leaderboard size)
 }
 
 // Function to display the leaderboard
 function displayLeaderboard() {
-    const leaderboardTable = document.getElementById('leaderboard-table');
-    leaderboardTable.innerHTML = '<tr><th>Player</th><th>Score</th><th>Level</th></tr>';
-
-    for (const entry of leaderboard) {
-        const row = document.createElement('tr');
-        row.innerHTML = `<td>${entry.player}</td><td>${entry.score}</td><td>${entry.level}</td>`;
-        leaderboardTable.appendChild(row);
-    }
+    // Implement leaderboard display logic here
+    // Create a table and populate it with the leaderboard data
 }
 
 // Function to clear the leaderboard
 function clearLeaderboard() {
-    leaderboard = [];
-    displayLeaderboard();
+    // Implement leaderboard clearing logic here
+    // Clear the leaderboard array and update the display
 }
 
-// Main menu
+// Function to play sound effects
+function playSoundEffect(soundFile) {
+    // Implement sound effect playing logic using Howler.js here
+}
+
+// Function to handle user input and game events
+function handleUserInput(input) {
+    // Implement user input handling logic here
+    // This can include handling guesses, player names, and more
+}
+
+// Main function
 function main() {
+    // Display the welcome screen initially
     displayWelcomeScreen();
-    selectGameLevel();
 }
 
 // Call the main function when the page loads
 window.onload = main;
-
-// Toggle Dark Mode
-const darkModeToggle = document.getElementById('dark-mode-toggle');
-darkModeToggle.addEventListener('click', () => {
-    const body = document.body;
-    body.classList.toggle('dark-mode');
-});
