fb051d7be73424e46fbb9055779edd2ebef2299e
iVGeek <48223278+iVGeek@users.noreply.github.com>
2023-09-17 16:08:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -43,14 +43,24 @@ function selectGameLevel() {
                 currentLevel = new GameLevel("Medium", 1, 50, 4, "yellow");
                 break;
         }
-        startGame();
+
+        // Enable the "Start Game" button
+        const startButton = document.getElementById('start-button');
+        startButton.disabled = false;
+
         levelSelect.style.display = 'none';
     });
+
+    // Enable the "Start Game" button initially
+    const startButton = document.getElementById('start-button');
+    startButton.disabled = false;
+
+    // Add click event listener to the "Start Game" button
+    startButton.addEventListener('click', startGame);
 }
 
 // Function to start the game
 function startGame() {
-    displayWelcomeScreen(); // Update welcome message
     secretNumber = generateRandomNumber(currentLevel.min, currentLevel.max);
     attemptsLeft = currentLevel.maxAttempts;
 
@@ -67,7 +77,93 @@ function startGame() {
     attemptsLeftElement.textContent = `Attempts left: ${attemptsLeft}`;
 }
 
-// ... (Rest of your existing code)
+// Function to generate a random number between min and max (inclusive)
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Function to handle user guesses
+function handleGuess() {
+    const guessInput = document.getElementById('guess-input');
+    const guess = parseInt(guessInput.value);
+
+    if (isNaN(guess) || guess < currentLevel.min || guess > currentLevel.max) {
+        displayMessage('Invalid guess. Please enter a number between ' + currentLevel.min + ' and ' + currentLevel.max + '.');
+        return;
+    }
+
+    attemptsLeft--;
+
+    if (guess === secretNumber) {
+        endGame(true);
+    } else if (attemptsLeft === 0) {
+        endGame(false);
+    } else {
+        displayMessage(guess < secretNumber ? 'Too low!' : 'Too high!');
+        displayAttemptsLeft();
+    }
+}
+
+// Function to end the game
+function endGame(isWinner) {
+    const messageElement = document.getElementById('message');
+    if (isWinner) {
+        messageElement.textContent = 'Congratulations! You guessed the number!';
+    } else {
+        messageElement.textContent = 'Game over. The number was ' + secretNumber + '.';
+    }
+
+    // Update leaderboard
+    updateLeaderboard('Player Name', currentLevel.maxAttempts - attemptsLeft, currentLevel.name);
+
+    // Hide game elements
+    const gameElements = document.getElementById('game-elements');
+    gameElements.style.display = 'none';
+
+    // Show level select again
+    const levelSelect = document.getElementById('level-select');
+    levelSelect.style.display = 'block';
+}
+
+// Function to display a message to the user
+function displayMessage(message) {
+    const messageElement = document.getElementById('message');
+    messageElement.textContent = message;
+}
+
+// Function to display remaining attempts
+function displayAttemptsLeft() {
+    const attemptsLeftElement = document.getElementById('attempts-left');
+    attemptsLeftElement.textContent = `Attempts left: ${attemptsLeft}`;
+}
+
+// Function to update the leaderboard
+function updateLeaderboard(player, score, level) {
+    leaderboard.push({ player, score, level });
+    leaderboard.sort((a, b) => a.score - b.score);
+    if (leaderboard.length > 5) {
+        leaderboard.shift(); // Remove the lowest score if leaderboard size exceeds the maximum
+    }
+    displayLeaderboard();
+}
+
+// Function to display the leaderboard
+function displayLeaderboard() {
+    const leaderboardTable = document.getElementById('leaderboard-table');
+    leaderboardTable.innerHTML = '<tr><th>Player</th><th>Score</th><th>Level</th></tr>';
+
+    for (const entry of leaderboard) {
+        const row = document.createElement('tr');
+        row.innerHTML = `<td>${entry.player}</td><td>${entry.score}</td><td>${entry.level}</td>`;
+        leaderboardTable.appendChild(row);
+    }
+}
+
+// Function to clear the leaderboard
+function clearLeaderboard() {
+    leaderboard = [];
+    displayLeaderboard();
+}
 
 // Main menu
 function main() {
