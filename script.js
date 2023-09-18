877b84ebb04cca27c8b714f15c1fa863c5be0dd2
iVGeek <48223278+iVGeek@users.noreply.github.com>
2023-09-18 17:53:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,178 +1,123 @@
 document.addEventListener('DOMContentLoaded', function () {
     const EASY_MIN = 1;
-    const EASY_MAX = 50;
+    const EASY_MAX = 20;
     const MEDIUM_MIN = 1;
-    const MEDIUM_MAX = 100;
+    const MEDIUM_MAX = 50;
     const HARD_MIN = 1;
-    const HARD_MAX = 500;
+    const HARD_MAX = 100;
 
-    let player1Name = 'Player 1';
-    let player2Name = 'Player 2';
-
+    const gameModeSelect = document.getElementById('gameMode');
+    const playerNameInputs = document.getElementById('playerNameInputs');
+    const player1NameInput = document.getElementById('player1Name');
+    const player2NameLabel = document.getElementById('player2NameLabel');
+    const player2NameInput = document.getElementById('player2NameInput');
     const difficultySelect = document.getElementById('difficulty');
-    const timeLimitInput = document.getElementById('timeLimit');
     const startButton = document.getElementById('startButton');
     const guessField = document.getElementById('guessField');
     const guessSubmit = document.getElementById('guessSubmit');
     const message = document.querySelector('.message');
-    const timeLeft = document.getElementById('timeLeft');
-    const attempts = document.getElementById('attempts');
-    const bestScoreDisplay = document.getElementById('bestScore');
-    const correctSound = document.getElementById('correctSound');
-    const wrongSound = document.getElementById('wrongSound');
-    const gameOverSound = document.getElementById('gameOverSound');
+    const winnerDisplay = document.querySelector('.winner');
+    const playerNameDisplay = document.getElementById('playerNameDisplay');
+    const attemptsLeftDisplay = document.getElementById('attemptsLeft');
 
-    let min, max, targetNumber, remainingAttempts, timer, bestScore, currentPlayer;
-    const playerGuesses = {
-        1: [],
-        2: [],
-    };
+    let min, max, targetNumber, remainingAttempts, currentPlayer;
+    let player1Name = '';
+    let player2Name = '';
+    let currentPlayerName = '';
+    let singlePlayer = true; // Single player mode flag
 
-    const timeLeftElement = document.getElementById('timeLeft');
-
-    startButton.addEventListener('click', startGame);
-    guessSubmit.addEventListener('click', function () {
-        checkGuess(currentPlayer);
+    gameModeSelect.addEventListener('change', function () {
+        const selectedGameMode = gameModeSelect.value;
+        if (selectedGameMode === 'single') {
+            playerNameInputs.classList.remove('hidden');
+            player2NameLabel.classList.add('hidden');
+            player2NameInput.classList.add('hidden');
+            singlePlayer = true;
+        } else {
+            playerNameInputs.classList.remove('hidden');
+            player2NameLabel.classList.remove('hidden');
+            player2NameInput.classList.remove('hidden');
+            singlePlayer = false;
+        }
+        startButton.classList.remove('hidden');
     });
 
-    function startGame() {
-        const gameMode = document.querySelector('input[name="gameMode"]:checked').value;
-
-        if (gameMode === 'single') {
-            player1Name = prompt('Enter your name (Single Player):') || 'Player 1';
-            player2Name = 'AI';
-            document.getElementById('player2Settings').style.display = 'none';
-        } else if (gameMode === 'multi') {
-            player1Name = prompt('Enter Player 1 name (Multiplayer):') || 'Player 1';
-            player2Name = prompt('Enter Player 2 name (Multiplayer):') || 'Player 2';
-            document.getElementById('player2Settings').style.display = 'block';
-        }
+    startButton.addEventListener('click', function () {
+        player1Name = player1NameInput.value.trim() || 'Player 1';
+        player2Name = player2NameInput.value.trim() || 'Player 2';
+        currentPlayerName = player1Name;
 
         const selectedDifficulty = difficultySelect.value;
         if (selectedDifficulty === 'easy') {
             min = EASY_MIN;
             max = EASY_MAX;
+            remainingAttempts = 4;
         } else if (selectedDifficulty === 'medium') {
             min = MEDIUM_MIN;
             max = MEDIUM_MAX;
+            remainingAttempts = 6;
         } else {
             min = HARD_MIN;
             max = HARD_MAX;
+            remainingAttempts = 8;
         }
 
         targetNumber = generateRandomNumber(min, max);
-        remainingAttempts = maxAttemptsByDifficulty(selectedDifficulty);
-        bestScore = parseInt(localStorage.getItem(selectedDifficulty)) || Infinity;
         currentPlayer = 1;
-        playerGuesses[1] = [];
-        playerGuesses[2] = [];
-
-        guessField.value = '';
-        message.textContent = '';
-        timeLeft.textContent = timeLimitInput.value;
-        attempts.textContent = '0';
-        bestScoreDisplay.textContent = bestScore === Infinity ? '-' : bestScore;
-
-        guessField.removeAttribute('disabled');
-        guessSubmit.removeAttribute('disabled');
-        timeLimitInput.setAttribute('disabled', 'disabled');
-        startButton.setAttribute('disabled', 'disabled');
-
-        startTimer();
-        displayCurrentPlayer();
-    }
+        resetUI();
+    });
 
     function generateRandomNumber(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
-    function maxAttemptsByDifficulty(difficulty) {
-        switch (difficulty) {
-            case 'easy':
-                return 10;
-            case 'medium':
-                return 7;
-            case 'hard':
-                return 5;
-            default:
-                return 10;
-        }
-    }
-
-    function startTimer() {
-        clearInterval(timer);
-        const timeLimit = parseInt(timeLimitInput.value);
-        let timeRemaining = timeLimit;
-
-        timer = setInterval(function () {
-            timeRemaining--;
-            timeLeft.textContent = timeRemaining;
-
-            if (timeRemaining === 0) {
-                endGame(0);
-            }
-        }, 1000);
-    }
-
-    function checkGuess(player) {
+    function makeGuess() {
         const userGuess = parseInt(guessField.value);
 
         if (isNaN(userGuess) || userGuess < min || userGuess > max) {
-            message.textContent = `${getPlayerName(player)}, please enter a valid number between ${min} and ${max}.`;
+            message.textContent = `Please enter a valid number between ${min} and ${max}.`;
             return;
         }
 
-        playerGuesses[player].push(userGuess);
+        remainingAttempts--;
+        attemptsLeftDisplay.textContent = remainingAttempts;
 
         if (userGuess === targetNumber) {
-            endGame(player);
-        } else if (playerGuesses[player].length >= remainingAttempts) {
-            endGame(0);
+            endGame(currentPlayerName);
+        } else if (remainingAttempts === 0) {
+            endGame('none');
         } else {
-            message.textContent = `${getPlayerName(player)}, your guess is recorded. Next player's turn.`;
-            guessField.value = '';
-            guessField.focus();
-            currentPlayer = 3 - currentPlayer;
-            displayCurrentPlayer();
+            currentPlayer = 3 - currentPlayer; // Switch players (1 <-> 2)
+            currentPlayerName = (currentPlayer === 1 || singlePlayer) ? player1Name : player2Name;
+            message.textContent = `${currentPlayerName}'s Turn`;
         }
     }
 
     function endGame(winner) {
-        clearInterval(timer);
         guessField.setAttribute('disabled', 'disabled');
         guessSubmit.setAttribute('disabled', 'disabled');
-        timeLimitInput.removeAttribute('disabled');
         startButton.removeAttribute('disabled');
 
-        if (winner === 1 || winner === 2) {
-            const winningPlayer = playerGuesses[1].length < playerGuesses[2].length ? 1 : 2;
-            message.textContent = `${getPlayerName(winningPlayer)} wins with ${playerGuesses[winningPlayer].length} attempts! The correct number was ${targetNumber}.`;
+        if (winner === 'none') {
+            message.textContent = `Both players lost. The correct number was ${targetNumber}.`;
+            winnerDisplay.textContent = '';
         } else {
-            message.textContent = `${getPlayerName(1)} and ${getPlayerName(2)} both lose. The correct number was ${targetNumber}.`;
-        }
-
-        if (playerGuesses[1].length < bestScore) {
-            bestScore = playerGuesses[1].length;
-            localStorage.setItem(difficultySelect.value, bestScore);
-            bestScoreDisplay.textContent = bestScore;
-        } else if (playerGuesses[2].length < bestScore) {
-            bestScore = playerGuesses[2].length;
-            localStorage.setItem(difficultySelect.value, bestScore);
-            bestScoreDisplay.textContent = bestScore;
-        }
-
-        if (winner === 1 || winner === 2) {
-            correctSound.play();
-        } else {
-            gameOverSound.play();
+            message.textContent = `${winner} wins! The correct number was ${targetNumber}.`;
+            winnerDisplay.textContent = `${winner} wins!`;
         }
     }
 
-    function getPlayerName(player) {
-        return player === 1 ? player1Name : player2Name;
+    function resetUI() {
+        guessField.value = '';
+        guessField.removeAttribute('disabled');
+        guessSubmit.removeAttribute('disabled');
+        message.textContent = `${currentPlayerName}'s Turn`;
+        winnerDisplay.textContent = '';
+        attemptsLeftDisplay.textContent = remainingAttempts;
+        playerNameDisplay.textContent = currentPlayerName;
     }
 
-    function displayCurrentPlayer() {
-        document.getElementById('currentPlayerInfo').textContent = `${getPlayerName(currentPlayer)}'s Turn`;
-    }
+    guessSubmit.addEventListener('click', function () {
+        makeGuess(); // Call the makeGuess function when the Submit Guess button is clicked
+    });
 });
