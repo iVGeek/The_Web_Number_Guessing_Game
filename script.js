3fdbd9c6b9ca22299d5fc010db1082d5d29a4963
iVGeek <48223278+iVGeek@users.noreply.github.com>
2023-09-18 19:03:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,179 +1,188 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const EASY_MIN = 1;
-    const EASY_MAX = 20;
-    const MEDIUM_MIN = 1;
-    const MEDIUM_MAX = 50;
-    const HARD_MIN = 1;
-    const HARD_MAX = 100;
+/* Add your CSS styles here */
 
-    const gameModeSelect = document.getElementById('gameMode');
-    const playerNameInputs = document.getElementById('playerNameInputs');
-    const player1NameInput = document.getElementById('player1Name');
-    const player2NameLabel = document.getElementById('player2NameLabel');
-    const player2NameInput = document.getElementById('player2NameInput');
-    const difficultySelect = document.getElementById('difficulty');
-    const startButton = document.getElementById('startButton');
-    const guessField = document.getElementById('guessField');
-    const guessSubmit = document.getElementById('guessSubmit');
-    const message = document.querySelector('.message');
-    const winnerDisplay = document.querySelector('.winner');
-    const playerNameDisplay = document.getElementById('playerNameDisplay');
-    const attemptsLeftDisplay = document.getElementById('attemptsLeft');
-    const leaderboardButton = document.getElementById('leaderboardButton');
-    const leaderboard = document.getElementById('leaderboard');
-    const leaderboardList = document.getElementById('leaderboardList');
-    const leaderboardCloseButton = document.getElementById('leaderboardCloseButton');
+body {
+    font-family: Arial, sans-serif;
+    text-align: center;
+    background-color: #f2f2f2;
+    margin: 0;
+    padding: 0;
+}
 
-    let min, max, targetNumber, remainingAttempts, currentPlayer;
-    let player1Name = '';
-    let player2Name = '';
-    let currentPlayerName = '';
-    let singlePlayer = true; // Single player mode flag
-    let leaderboardData = [];
+h1 {
+    color: #333;
+}
 
-    gameModeSelect.addEventListener('change', function () {
-        const selectedGameMode = gameModeSelect.value;
-        if (selectedGameMode === 'single') {
-            playerNameInputs.classList.remove('hidden');
-            player2NameLabel.classList.add('hidden');
-            player2NameInput.classList.add('hidden');
-            singlePlayer = true;
-        } else {
-            playerNameInputs.classList.remove('hidden');
-            player2NameLabel.classList.remove('hidden');
-            player2NameInput.classList.remove('hidden');
-            singlePlayer = false;
-        }
-        startButton.classList.remove('hidden');
-    });
+.game-settings {
+    margin: 20px auto;
+    width: 80%;
+    max-width: 400px;
+    background-color: #fff;
+    border: 1px solid #ddd;
+    padding: 10px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
 
-    startButton.addEventListener('click', function () {
-        player1Name = player1NameInput.value.trim() || 'Player 1';
-        player2Name = player2NameInput.value.trim() || 'Player 2';
-        currentPlayerName = player1Name;
+.game-settings label {
+    display: block;
+    margin-bottom: 5px;
+    font-weight: bold;
+}
 
-        const selectedDifficulty = difficultySelect.value;
-        if (selectedDifficulty === 'easy') {
-            min = EASY_MIN;
-            max = EASY_MAX;
-            remainingAttempts = 4;
-        } else if (selectedDifficulty === 'medium') {
-            min = MEDIUM_MIN;
-            max = MEDIUM_MAX;
-            remainingAttempts = 6;
-        } else {
-            min = HARD_MIN;
-            max = HARD_MAX;
-            remainingAttempts = 8;
-        }
+.game-settings select,
+.game-settings input[type="text"] {
+    width: 100%;
+    padding: 8px;
+    margin-bottom: 10px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+}
 
-        targetNumber = generateRandomNumber(min, max);
-        currentPlayer = 1;
-        resetUI();
-    });
+.game-settings button {
+    padding: 10px 20px;
+    background-color: #007BFF;
+    color: #fff;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
 
-    function generateRandomNumber(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
+.game-settings button.hidden {
+    display: none;
+}
 
-    function makeGuess() {
-        const userGuess = parseInt(guessField.value);
+.gameplay {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    margin: 20px auto;
+    width: 80%;
+    max-width: 400px;
+    background-color: #fff;
+    border: 1px solid #ddd;
+    padding: 20px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
 
-        if (isNaN(userGuess) || userGuess < min || userGuess > max) {
-            message.textContent = `Please enter a valid number between ${min} and ${max}.`;
-            return;
-        }
+.player-info p {
+    margin: 0;
+    font-weight: bold;
+}
 
-        remainingAttempts--;
-        attemptsLeftDisplay.textContent = remainingAttempts;
+.game-controls {
+    margin-top: 20px;
+}
 
-        if (userGuess === targetNumber) {
-            endGame(currentPlayerName);
-        } else if (remainingAttempts === 0) {
-            endGame('none');
-        } else {
-            currentPlayer = 3 - currentPlayer; // Switch players (1 <-> 2)
-            currentPlayerName = (currentPlayer === 1 || singlePlayer) ? player1Name : player2Name;
-            message.textContent = `${currentPlayerName}'s Turn`;
-        }
-    }
+.guess-field {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 10px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+}
 
-    function endGame(winner) {
-        guessField.setAttribute('disabled', 'disabled');
-        guessSubmit.setAttribute('disabled', 'disabled');
-        startButton.removeAttribute('disabled');
+.guess-submit {
+    padding: 10px 20px;
+    background-color: #28a745;
+    color: #fff;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
 
-        if (winner === 'none') {
-            message.textContent = `Both players lost. The correct number was ${targetNumber}.`;
-            winnerDisplay.textContent = '';
-        } else {
-            message.textContent = `${winner} wins! The correct number was ${targetNumber}.`;
-            winnerDisplay.textContent = `${winner} wins!`;
+.message {
+    margin-top: 20px;
+    font-weight: bold;
+}
 
-            // Add the winner to the leaderboard data
-            leaderboardData.push({
-                name: winner,
-                attempts: remainingAttempts,
-                difficulty: difficultySelect.value,
-                entryTime: new Date().toLocaleString(), // Entry time in a readable format
-            });
+.winner {
+    margin-top: 10px;
+    font-weight: bold;
+    color: #28a745;
+}
 
-            // Sort the leaderboard data by attempts (lower attempts are better)
-            leaderboardData.sort((a, b) => a.attempts - b.attempts);
+/* Leaderboard styles */
+#leaderboard {
+    background-color: #fff;
+    border: 1px solid #ddd;
+    padding: 20px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    z-index: 1;
+    text-align: left;
+}
 
-            // Update the leaderboard UI
-            updateLeaderboard();
-        }
-    }
+#leaderboard h2 {
+    margin: 0;
+    font-weight: bold;
+    font-size: 1.5rem;
+}
 
-    function resetUI() {
-        guessField.value = '';
-        guessField.removeAttribute('disabled');
-        guessSubmit.removeAttribute('disabled');
-        message.textContent = `${currentPlayerName}'s Turn`;
-        winnerDisplay.textContent = '';
-        attemptsLeftDisplay.textContent = remainingAttempts;
-        playerNameDisplay.textContent = currentPlayerName;
-    }
+#leaderboardList {
+    list-style: none;
+    padding: 0;
+}
 
-    guessSubmit.addEventListener('click', function () {
-        makeGuess(); // Call the makeGuess function when the Submit Guess button is clicked
-    });
+#leaderboardList li {
+    margin-bottom: 10px;
+}
 
-    // Leaderboard button click event
-    leaderboardButton.addEventListener('click', function () {
-        // Display the leaderboard
-        leaderboard.style.display = 'block';
+#leaderboardCloseButton {
+    background-color: #dc3545;
+    color: #fff;
+    border: none;
+    border-radius: 4px;
+    padding: 5px 10px;
+    font-size: 0.9rem;
+    cursor: pointer;
+}
 
-        // Update the leaderboard UI
-        updateLeaderboard();
-    });
+#leaderboardCloseButton:hover {
+    background-color: #c82333;
+}
 
-    // Leaderboard close button click event
-    leaderboardCloseButton.addEventListener('click', function () {
-        // Hide the leaderboard
-        leaderboard.style.display = 'none';
-    });
+/* Dark Mode Styles */
+.dark-mode {
+    background-color: #333;
+    color: #fff;
+}
 
-    function updateLeaderboard() {
-        leaderboardList.innerHTML = ''; // Clear previous leaderboard entries
+.dark-mode h1 {
+    color: #fff;
+}
 
-        // Add leaderboard entries to the UI
-        leaderboardData.forEach((entry, index) => {
-            const listItem = document.createElement('li');
-            listItem.innerHTML = `<span style="color: ${getRandomColor()};">${entry.name}</span> - ` +
-                `Attempts: ${entry.attempts}, Difficulty: ${entry.difficulty}, ` +
-                `Entry Time: ${entry.entryTime}`;
-            leaderboardList.appendChild(listItem);
-        });
-    }
+.dark-mode .game-settings,
+.dark-mode .gameplay {
+    background-color: #444;
+    border: 1px solid #666;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
 
-    function getRandomColor() {
-        const letters = '0123456789ABCDEF';
-        let color = '#';
-        for (let i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
-    }
-});
+.dark-mode .game-settings label,
+.dark-mode .game-settings button,
+.dark-mode .gameplay .player-info p,
+.dark-mode .gameplay .game-controls,
+.dark-mode .guess-field,
+.dark-mode .guess-submit,
+.dark-mode .message,
+.dark-mode .winner,
+.dark-mode #leaderboard {
+    color: #fff;
+}
+
+.dark-mode #leaderboard {
+    background-color: #444;
+    border: 1px solid #666;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+.dark-mode #leaderboardCloseButton {
+    background-color: #dc3545;
+}
+
+.dark-mode #leaderboardCloseButton:hover {
+    background-color: #c82333;
+}
