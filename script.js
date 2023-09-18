63a77aa0e184a3c2a39f7623798c74d1338bf28d
iVGeek <48223278+iVGeek@users.noreply.github.com>
2023-09-18 19:40:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -23,6 +23,11 @@ document.addEventListener('DOMContentLoaded', function () {
     const leaderboard = document.getElementById('leaderboard');
     const leaderboardList = document.getElementById('leaderboardList');
     const leaderboardCloseButton = document.getElementById('leaderboardCloseButton');
+    const sortCriteriaSelect = document.getElementById('sortCriteria');
+    const filterDifficultySelect = document.getElementById('filterDifficulty');
+    const prevPageButton = document.getElementById('prevPageButton');
+    const nextPageButton = document.getElementById('nextPageButton');
+    const currentPageDisplay = document.getElementById('currentPage');
 
     let min, max, targetNumber, remainingAttempts, currentPlayer;
     let player1Name = '';
@@ -30,6 +35,8 @@ document.addEventListener('DOMContentLoaded', function () {
     let currentPlayerName = '';
     let singlePlayer = true; // Single player mode flag
     let leaderboardData = [];
+    let currentPage = 1;
+    const leaderboardPerPage = 5;
 
     gameModeSelect.addEventListener('change', function () {
         const selectedGameMode = gameModeSelect.value;
@@ -117,11 +124,26 @@ document.addEventListener('DOMContentLoaded', function () {
                 difficulty: difficultySelect.value,
             });
 
-            // Sort the leaderboard data by attempts (lower attempts are better)
-            leaderboardData.sort((a, b) => a.attempts - b.attempts);
+            // Sort the leaderboard data by selected criteria
+            const sortCriteria = sortCriteriaSelect.value;
+            if (sortCriteria === 'attempts') {
+                leaderboardData.sort((a, b) => a.attempts - b.attempts);
+            } else if (sortCriteria === 'difficulty') {
+                leaderboardData.sort((a, b) => a.difficulty.localeCompare(b.difficulty));
+            }
+
+            // Filter the leaderboard data by difficulty if needed
+            const filterDifficulty = filterDifficultySelect.value;
+            if (filterDifficulty !== 'all') {
+                leaderboardData = leaderboardData.filter(entry => entry.difficulty === filterDifficulty);
+            }
 
             // Update the leaderboard UI
             updateLeaderboard();
+
+            // Enable pagination buttons
+            prevPageButton.removeAttribute('disabled');
+            nextPageButton.removeAttribute('disabled');
         }
     }
 
@@ -154,14 +176,61 @@ document.addEventListener('DOMContentLoaded', function () {
         leaderboard.style.display = 'none';
     });
 
+    // Sort and filter change events
+    sortCriteriaSelect.addEventListener('change', updateLeaderboard);
+    filterDifficultySelect.addEventListener('change', updateLeaderboard);
+
+    // Pagination button click events
+    prevPageButton.addEventListener('click', function () {
+        if (currentPage > 1) {
+            currentPage--;
+            updateLeaderboard();
+        }
+    });
+
+    nextPageButton.addEventListener('click', function () {
+        const maxPage = Math.ceil(leaderboardData.length / leaderboardPerPage);
+        if (currentPage < maxPage) {
+            currentPage++;
+            updateLeaderboard();
+        }
+    });
+
     function updateLeaderboard() {
         leaderboardList.innerHTML = ''; // Clear previous leaderboard entries
 
-        // Add leaderboard entries to the UI
-        leaderboardData.forEach((entry, index) => {
+        // Calculate the start and end indices for the current page
+        const startIndex = (currentPage - 1) * leaderboardPerPage;
+        const endIndex = startIndex + leaderboardPerPage;
+
+        // Add leaderboard entries to the UI for the current page
+        leaderboardData.slice(startIndex, endIndex).forEach((entry, index) => {
             const listItem = document.createElement('li');
-            listItem.textContent = `#${index + 1}: ${entry.name} - ${entry.attempts} attempts (${entry.difficulty})`;
+            listItem.textContent = `#${startIndex + index + 1}: ${entry.name} - ${entry.attempts} attempts (${entry.difficulty})`;
+
+            // Highlight the current user's entry
+            if (entry.name === currentPlayerName) {
+                listItem.classList.add('current-user');
+            }
+
             leaderboardList.appendChild(listItem);
         });
+
+        // Update current page display
+        currentPageDisplay.textContent = `Page ${currentPage}`;
+
+        // Disable pagination buttons when at the start or end of the leaderboard
+        if (currentPage === 1) {
+            prevPageButton.setAttribute('disabled', 'disabled');
+        } else {
+            prevPageButton.removeAttribute('disabled');
+        }
+
+        const maxPage = Math.ceil(leaderboardData.length / leaderboardPerPage);
+        if (currentPage === maxPage) {
+            nextPageButton.setAttribute('disabled', 'disabled');
+        } else {
+            nextPageButton.removeAttribute('disabled');
+        }
     }
 });
